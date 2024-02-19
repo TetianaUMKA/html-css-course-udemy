5ef5ca1dac09037b41e9db0ff18618dd1b20954d
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-02-19 17:58:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,6 @@ const likeBtn = document.querySelector(".like-btn");
 
 likeBtn.addEventListener("click", function () {
   likeBtn.textContent = "🥰";
+  likeBtn.style.border = "1px solid transparent";
+  likeBtn.style.boxShadow = "none";
 });
